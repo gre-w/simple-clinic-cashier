7aaaaf0fe182df96639cc17a0ec900b2038dae49
Gregorius Y <137756897+gre-w@users.noreply.github.com>
2023-10-31 00:25:44 +0800

    Database

---
+++ Data.java
@@ -0,0 +1,28 @@
+public class Data {
+    public static String[] pasien = {"Pasien 1", "Pasien 2", "Pasien 3", "Pasien 4"};
+
+    public static String[] gejala = {"Demam", "Menggigil", "Nyeri", "Kelelahan", "Bersin - Bersin", "Batuk", "Hidung Tersumbat", "Sakit Kepala", "Sakit Tenggorokan", "Sesak Nafas", "Diare"};
+
+    public static String[] penyakit = {"Masuk Angin", "Flu", "Salesma", "Alergi"};
+
+    public static String[] obat = {
+        "Acetylcysteine 200 mg 10 Kapsul = 60000",
+        "Sanadryl DMP Sirup 120 ml = 75000",
+        "Demacolin 10 Tablet = 30000",
+        "Antangin JRG Cair 12 Sachet = 50000",
+        "Moxic Forte 15mg Tablet = 50000",
+        "Lerzin 10 mg 10 Kapsul = 75000"
+    };
+
+    public static String[][] gejalaPenyakit = {
+        {"Demam", "Nyeri", "Kelelahan", "Batuk", "Hidung Tersumbat", "Sakit Tenggorokan", "Sesak Nafas", "Diare"},
+        {"Demam", "Menggigil", "Nyeri", "Kelelahan", "Bersin - Bersin", "Batuk", "Hidung Tersumbat", "Sakit Kepala", "Sakit Tenggorokan", "Sesak Nafas", "Diare"},
+        {"Demam", "Nyeri", "Kelelahan", "Bersin - Bersin", "Batuk", "Hidung Tersumbat", "Sesak Nafas"},
+        {"Nyeri", "Kelelahan", "Bersin - Bersin", "Batuk", "Hidung Tersumbat", "Sakit Tenggorokan", "Sesak Nafas", "Diare"}
+    };
+    
+    public static String[] obatFlu = {"Acetylcysteine 200 mg 10 Kapsul = 60000", "Sanadryl DMP Sirup 120 ml = 75000", "Demacolin 10 Tablet = 30000"};
+    public static String[] obatMasukAngin = {"Antangin JRG Cair 12 Sachet = 50000", "Moxic Forte 15mg Tablet = 50000"};
+    public static String[] obatSalesma = {"Acetylcysteine 200 mg 10 Kapsul = 60000", "Demacolin 10 Tablet = 30000", "Moxic Forte 15mg Tablet = 50000"};
+    public static String[] obatAlergi = {"Acetylcysteine 200 mg 10 Kapsul = 60000", "Demacolin 10 Tablet = 30000", "Lerzin 10 mg 10 Kapsul = 75000"};
+}
